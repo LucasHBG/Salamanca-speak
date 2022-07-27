768bb0d5b171140f8af773eb674acd866b3203ef
Lucas Guimarães <lukahbg@gmail.com>
2022-07-26 22:18:51 -0300

    [FEAT]:  concluded Salamancaspeak implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -53,43 +53,91 @@ export class HomeComponent implements OnInit {
     this.formGroup
       .get("entryText")
       ?.valueChanges.subscribe((currentValue: string) => {
-        console.log("entryText value changed");
-        // console.log(currentValue);
+        // console.log("entryText value changed");
 
-        if (currentValue.length > 0) {
-          // Iterate through currentValue string
-          for (let i = 0; i < currentValue.length; i++) {
+        setTimeout(() => {
+          if (currentValue.length > 0) {
+            // Iterate through currentValue string
 
-            //Verifies if value is part of alphabet
-            if (
-              currentValue.charAt(i) >= ("a" || "A") &&
-              currentValue.charAt(i) <= ("z" || "Z")
-            ) {
+            // console.log("Tamanho currentVal: " + currentValue.length);
+            // console.log("Tamanho outputTexts [INICIO]: " + this.outputTexts.length);
+
+            //Reset array if the user erased some value
+            if (this.outputTexts.length > currentValue.length) {
+              this.outputTexts.pop();
+            } else {
+              //Verifies if value is part of alphabet
               currentValue = this.uppercasePipe.transform(currentValue);
-              let phrase: string[] = [];
+              if (
+                currentValue.charAt(currentValue.length - 1) >= "A" &&
+                currentValue.charAt(currentValue.length - 1) <= "Z"
+              ) {
+                let phrase: string[] = [];
 
-              //Search for letter at the alphabet
-              for (let i = 0; i < this.alphabet.length; i++) {
-                phrase = phrase.concat(this.alphabet[i] + " ");
-                if (
-                  currentValue.charAt(currentValue.length - 1) ===
-                  this.alphabet[i]
-                ) {
-                  phrase = phrase.concat("*DING*");
-                  break;
+                //Search for letter at the alphabet
+                for (let j = 0; j < this.alphabet.length; j++) {
+                  phrase = phrase.concat(this.alphabet[j] + " ");
+                  if (
+                    currentValue.charAt(currentValue.length - 1) ===
+                    this.alphabet[j]
+                  ) {
+                    phrase = phrase.concat("*DING*");
+                    this.outputTexts.push(phrase.join(" "));
+                    break;
+                  }
                 }
+                // console.log(phrase);
               }
-              console.log(phrase);
 
-              //Reset array if the user erased some value
-              if (this.outputTexts.length < currentValue.length) {
-                this.outputTexts = [""];
-              } else {
-                this.outputTexts.push(phrase.join(" "));
-              }
+              // console.log("Tamanho outputTexts [FIM]: " + this.outputTexts.length);
+
+            }
+          } else {
+            //remove any value left on the array 
+            if (this.outputTexts.length > currentValue.length) {
+              this.outputTexts.pop();
             }
           }
-        }
+
+          // if (currentValue.length > 0) {
+          //   // Iterate through currentValue string
+          //   for (let i = 0; i < currentValue.length; i++) {
+          //     console.log("Tamanho currentVal: " + currentValue.length);
+          //     console.log("INDEX 0 currentVal: " + currentValue.charAt(0));
+          //     if(currentValue.length == 2)
+          //       console.log("INDEX 1 currentVal: " + currentValue.charAt(1));
+
+          //     //Verifies if value is part of alphabet
+          //     currentValue = this.uppercasePipe.transform(currentValue);
+          //     if (
+          //       currentValue.charAt(i) >= "A" &&
+          //       currentValue.charAt(i) <= "Z"
+          //     ) {
+          //       let phrase: string[] = [];
+
+          //       //Search for letter at the alphabet
+          //       for (let j = 0; j < this.alphabet.length; j++) {
+          //         phrase = phrase.concat(this.alphabet[i] + " ");
+          //         if (
+          //           currentValue.charAt(currentValue.length - 1) ===
+          //           this.alphabet[j]
+          //         ) {
+          //           phrase = phrase.concat("*DING*");
+          //           this.outputTexts.push(phrase.join(" "));
+          //           break;
+          //         }
+
+          //       }
+          //       console.log(phrase);
+
+          //       //Reset array if the user erased some value
+          //       if (this.outputTexts.length < currentValue.length) {
+          //         this.outputTexts.splice(0);
+          //       }
+          //     }
+          //   }
+          // }
+        }, 700);
 
         // setTimeout(() => {
         //   console.log(this.formGroup.get("entryText")?.value);
